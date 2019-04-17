6ec79b9355cff0e216176d655c3ce56ed9fe6f22
houlin131110 <houlin@shougongke.com>
2019-04-17 10:51:32 +0800

    修改gulpfile文件

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 var minifycss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
 var babel = require('gulp-babel');
@@ -9,8 +10,8 @@ var babel = require('gulp-babel');
 gulp.task('script', function() {
     return gulp.src(['js/*.js','!js/*.min.js'])
         .pipe(babel())
-        .pipe(gulp.dest('dist/js'))    //输出main.js到文件夹
         .pipe(uglify())    //压缩
+        .pipe(gulp.dest('dist/js'))    //输出main.js到文件夹
 });
 gulp.task('auto',function(){
     gulp.watch(['js/*.js','!js/*.min.js'],['script']);
@@ -19,13 +20,14 @@ gulp.task('auto',function(){
 });
 gulp.task('css', function() {
     return gulp.src('css/*.css')      //压缩的文件
+        /*.pipe(minifycss()); */  //执行压缩
+        .pipe(cleanCSS({compatibility: 'ie7'}))
         .pipe(gulp.dest('dist/css'))   //输出文件夹
-        .pipe(minifycss());   //执行压缩
 });
 gulp.task('images', function() {
     return gulp.src('images/*.*')      //压缩的文件
+        .pipe(imagemin())  //执行压缩
         .pipe(gulp.dest('dist/images'))   //输出文件夹
-        .pipe(imagemin());   //执行压缩
 });
 gulp.task('default',['script','auto','css','images'],function () {
     console.log("default");
